38560de3a997b238ee90cd0d582952636b032c0e
Faith Kiprono <78324506+faithckip@users.noreply.github.com>
2024-02-23 12:52:04 +0100

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Ask for user's name, age, and location
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+# Print personalized message
+print(f"Hello  my name is {name}, I am {age} years old and live in {location}.")
