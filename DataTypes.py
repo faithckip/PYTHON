114d6793e2e060aeda5c1f73909d7f4406da8f8e
faithckip <faithckiprono@gmail.com>
2024-02-28 14:15:51 +0100

    first commit

---
+++ DataTypes.py
@@ -0,0 +1,25 @@
+a=100
+print("The type of variable having value", a, "is ", type(a))
+print(a)
+b=True
+print(b)
+print("The type of variable having value", b, "is",  type(b))
+c= 30.90
+print(c)
+print("The type of variable having value", c, "is",  type(c))
+d=10+7j
+print(d)
+print("The type of variable having value", d, "is",  type(d))
+Building_List = [450, "wheelbarrows", 180, "shovels", "bread", "thread"]
+print(Building_List)
+print(Building_List[1:5])
+print(Building_List[4])
+plp_tuple="abcd", 786, 2.23, "John", 70.2  
+
+print(plp_tuple)
+tiny_tuple = "cake", 478
+print(tiny_tuple)
+print(tiny_tuple + plp_tuple)
+dict={}
+dict[4] ="My name is kiprono"
+print(dict[4])
